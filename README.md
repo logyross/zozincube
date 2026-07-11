70fbbfe3ee3416768a99d50de2296b9af1800f6e
logyross <yakrebt99@gmail.com>
2026-07-12 02:27:07 +0300

    basic grid drawing as ppm file.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # zozincube
 A software rendered spinning wire cube in C++ inspired by tsoding's video
+
+# steps
+- make every pixel a point in a 2D boolean array
+- markPoint method that takes x, y, size. finds the x,y point and marks a "size" sized square
+- draw the entire board, marked pixels are a different color
+
+# next
+- now do this on a loop, transform the marked x,y coords for every frame and create movement
+- implement the tsoding algorithm 
--- zozincube.cpp
+++ zozincube.cpp
@@ -1,5 +1,96 @@
+#include <fstream>
+#include <iostream>
+#include <array>
+
+constexpr int w = 16*60;
+constexpr int h = 9*60;
+
+struct Cell
+{
+
+	int color_r{};
+	int color_g{};
+	int color_b{};
+};
+
+struct Color 
+{
+	int r{};
+	int g{};
+	int b{};
+};
+
+struct Coord
+{
+	int x{};
+	int y{};
+};
+
+// void fillRect(std::ofstream& f, Coord coord, Color color, int w, int h)
+// {
+
+// 	for (int i=0; i <= w; i++)
+// 	{
+// 		for (int j=0; j <= h; j++)
+// 		{
+// 			f.put(0x10);
+// 			f.put(0x10);
+// 			f.put(0x10);
+// 		}
+// 	}
+// }
+
+void markPoint(int x, int y, int size, std::array<std::array<bool, h>, w> &map)
+{
+	for (int i = x; i < x + size; i++)
+	{
+		for (int j = y; j < y + size; j++)
+		{
+			map[static_cast<size_t>(i)][static_cast<size_t>(j)] = true;
+		}
+	}
+}
+
 
 int main()
 {
+	std::ofstream f{"test.ppm"};
+
+	if (!f) 
+	{
+		std::cerr << "failed to open file.\n";
+		return 1;
+	}
+
+	f << "P6\n";
+
+
+	std::array<std::array<bool, h>, w> map{};
+
+	markPoint(16*30, 9*30, 10, map);
+	markPoint(16*40, 9*40, 20, map);
+	
+
+	f << w << " " << h << "\n";
+	f << "255\n";
+
+	for (int y=0; y < h; y++)
+	{
+		for (int x=0; x < w; x++)
+		{
+			if (map[static_cast<size_t>(x)][static_cast<size_t>(y)]) 
+			{
+				f.put(0xFF);
+				f.put(0x10);
+				f.put(0x10);
+			}
+			else
+			{
+				f.put(0x10);
+				f.put(0x10);
+				f.put(0x10);
+			}
+		}
+	}
 	return 0;
 }
