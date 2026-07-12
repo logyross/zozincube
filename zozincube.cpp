33f206e2ca84a543ac0bed4e99177bf9bf036dd0
logyross <yakrebt99@gmail.com>
2026-07-12 17:13:49 +0300

    rotate the points via vector rotation.

--- zozincube.cpp
+++ zozincube.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 #include <array>
 #include <string>
+#include <cmath>
+#define _USE_MATH_DEFINES
 
 constexpr double w = 16*60;
-constexpr double h = 9*60;
+constexpr double h = 16*60;
 constexpr double fps = 60;
 constexpr double dt = 1/fps;
 
@@ -33,8 +35,8 @@ void markPoint(Coord coord, int size, std::array<std::array<bool, (int)h>, (int)
 	{
 		for (double y = coord.y; y < coord.y + size; y++)
 		{
-			if (x <= w && x >= 0
-			&& y <= h && y >= 0)
+			if (x < w && x >= 0
+			&& y < h && y >= 0)
 			{
 				map[static_cast<size_t>(x)][static_cast<size_t>(y)] = true;
 			}
@@ -47,22 +49,58 @@ Coord project(Coord3D coord)
 	return {coord.x/coord.z, coord.y/coord.z};
 }
 
-void handle_point(Coord3D coord, std::array<std::array<bool, (int)h>, (int)w> &map)
+Coord3D translatePoint(Coord3D coord, double dz)
 {
-	Coord projected_coord = project({coord.x, coord.y, coord.z});
+	return {coord.x, coord.y, coord.z+dz};
+}
+
+Coord3D rotate_xz(Coord3D coord, double angle)
+{
+	const double c = std::cos(angle);
+	const double s = std::sin(angle);
+	return {
+		coord.x*c-coord.z*s,
+		coord.y,
+		coord.x*s+coord.z*c
+	};
+}
+
+void handlePoint(Coord3D coord, std::array<std::array<bool, (int)h>, (int)w> &map, double dz, double angle)
+{
+	std::cout << "-- HANDLE POINT -- \n";
+	Coord3D rotated_point = rotate_xz(coord, angle);
+	Coord3D translated_point = translatePoint(rotated_point, dz);
+	std::cout << "TRANSLATED -> X: " << translated_point.x << " Y: " << translated_point.y << "\n";
+	Coord projected_coord = project(translated_point);
+	std::cout << "PROJECTED -> X: " << projected_coord.x << " Y: " << projected_coord.y << "\n";
 	Coord normalized_coord = normalize(projected_coord);
-	// std::cout << "X: " << normalized_coord.x << " Y: " << normalized_coord.y << "\n";
-	// std::cout << "Z: " << 1.0 + dz << "\n";
+	std::cout << "NORMALIZED -> X: " << normalized_coord.x << " Y: " << normalized_coord.y << "\n";
 	markPoint(normalized_coord, 20, map);
 }
 
+
+
+const Coord3D vs[]
+{
+	{ 0.25,  0.25,   0.25},
+	{-0.25,  0.25,   0.25},
+	{ 0.25, -0.25,   0.25},
+	{-0.25, -0.25,   0.25},
+	{ 0.25,  0.25,  -0.25},
+	{-0.25,  0.25,  -0.25},
+	{ 0.25, -0.25,  -0.25},
+	{-0.25, -0.25,  -0.25},
+};
+
 int main()
 {
-	double dz = 0.0;
+	double dz = 1.0;
+	double angle = 0.0;
 	std::cout << "W: " << w << " H: " << h << "\n";
-	for (int t=0; t < 100; t++)
+	for (int t=0; t < 60; t++)
 	{
-		dz += 1*dt;
+		//dz += 1*dt;
+		angle += 2*M_PI*dt;
     		std::string fileName = "output/test-" + std::to_string(t) + ".ppm";
 		std::ofstream f{fileName};
 
@@ -76,10 +114,14 @@ int main()
 
 		std::array<std::array<bool, (int)h>, (int)w> map{};
 
-		handle_point({0.5, 0.5, 1.0 + dz}, map);
-		handle_point({-0.5, 0.5, 1.0 + dz}, map);
-		handle_point({0.5, -0.5, 1.0 + dz}, map);
-		handle_point({-0.5, -0.5, 1.0 + dz}, map);
+		for (Coord3D point : vs)
+		{
+			handlePoint(point, map, dz, angle);
+		}
+		// handlePoint({0.5, 0.5, 1.0 + dz}, map, dz);
+		// handlePoint({-0.5, 0.5, 1.0 + dz}, map, dz);
+		// handlePoint({0.5, -0.5, 1.0 + dz}, map, dz);
+		// handlePoint({-0.5, -0.5, 1.0 + dz}, map, dz);
 
 		f << w << " " << h << "\n";
 		f << "255\n";
