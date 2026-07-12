299bed22adf36c1419a23a61fafaca040d2fd373
logyross <yakrebt99@gmail.com>
2026-07-12 16:14:02 +0300

    Projection of 3D points into 2D surface.

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+ffmpeg -i output/test-%d.ppm -r 60 output.mp4
+rm output/*.ppm
+mpv output.mp4
--- zozincube.cpp
+++ zozincube.cpp
@@ -1,94 +1,105 @@
 #include <fstream>
 #include <iostream>
 #include <array>
+#include <string>
 
-constexpr int w = 16*60;
-constexpr int h = 9*60;
+constexpr double w = 16*60;
+constexpr double h = 9*60;
+constexpr double fps = 60;
+constexpr double dt = 1/fps;
 
-struct Cell
+struct Coord 
 {
-
-	int color_r{};
-	int color_g{};
-	int color_b{};
+	double x{};
+	double y{};
 };
 
-struct Color 
+struct Coord3D
 {
-	int r{};
-	int g{};
-	int b{};
+	double x{};
+	double y{};
+	double z{};
 };
 
-struct Coord
+Coord normalize(Coord coord)
 {
-	int x{};
-	int y{};
-};
+	return {(coord.x+1)/2.0*w, 
+		(1 - (coord.y+1)/2.0)*h};
+}
 
-// void fillRect(std::ofstream& f, Coord coord, Color color, int w, int h)
-// {
-
-// 	for (int i=0; i <= w; i++)
-// 	{
-// 		for (int j=0; j <= h; j++)
-// 		{
-// 			f.put(0x10);
-// 			f.put(0x10);
-// 			f.put(0x10);
-// 		}
-// 	}
-// }
-
-void markPoint(int x, int y, int size, std::array<std::array<bool, h>, w> &map)
+void markPoint(Coord coord, int size, std::array<std::array<bool, (int)h>, (int)w> &map)
 {
-	for (int i = x; i < x + size; i++)
+	for (double x = coord.x; x < coord.x + size; x++)
 	{
-		for (int j = y; j < y + size; j++)
+		for (double y = coord.y; y < coord.y + size; y++)
 		{
-			map[static_cast<size_t>(i)][static_cast<size_t>(j)] = true;
+			if (x <= w && x >= 0
+			&& y <= h && y >= 0)
+			{
+				map[static_cast<size_t>(x)][static_cast<size_t>(y)] = true;
+			}
 		}
 	}
 }
 
+Coord project(Coord3D coord)
+{
+	return {coord.x/coord.z, coord.y/coord.z};
+}
+
+void handle_point(Coord3D coord, std::array<std::array<bool, (int)h>, (int)w> &map)
+{
+	Coord projected_coord = project({coord.x, coord.y, coord.z});
+	Coord normalized_coord = normalize(projected_coord);
+	// std::cout << "X: " << normalized_coord.x << " Y: " << normalized_coord.y << "\n";
+	// std::cout << "Z: " << 1.0 + dz << "\n";
+	markPoint(normalized_coord, 20, map);
+}
 
 int main()
 {
-	std::ofstream f{"test.ppm"};
-
-	if (!f) 
+	double dz = 0.0;
+	std::cout << "W: " << w << " H: " << h << "\n";
+	for (int t=0; t < 100; t++)
 	{
-		std::cerr << "failed to open file.\n";
-		return 1;
-	}
+		dz += 1*dt;
+    		std::string fileName = "output/test-" + std::to_string(t) + ".ppm";
+		std::ofstream f{fileName};
 
-	f << "P6\n";
-
-
-	std::array<std::array<bool, h>, w> map{};
-
-	markPoint(16*30, 9*30, 10, map);
-	markPoint(16*40, 9*40, 20, map);
-	
-
-	f << w << " " << h << "\n";
-	f << "255\n";
-
-	for (int y=0; y < h; y++)
-	{
-		for (int x=0; x < w; x++)
+		if (!f) 
 		{
-			if (map[static_cast<size_t>(x)][static_cast<size_t>(y)]) 
+			std::cerr << "failed to open file.\n";
+			return 1;
+		}
+
+		f << "P6\n";
+
+		std::array<std::array<bool, (int)h>, (int)w> map{};
+
+		handle_point({0.5, 0.5, 1.0 + dz}, map);
+		handle_point({-0.5, 0.5, 1.0 + dz}, map);
+		handle_point({0.5, -0.5, 1.0 + dz}, map);
+		handle_point({-0.5, -0.5, 1.0 + dz}, map);
+
+		f << w << " " << h << "\n";
+		f << "255\n";
+
+		for (int y=0; y < h; y++)
+		{
+			for (int x=0; x < w; x++)
 			{
-				f.put(0xFF);
-				f.put(0x10);
-				f.put(0x10);
-			}
-			else
-			{
-				f.put(0x10);
-				f.put(0x10);
-				f.put(0x10);
+				if (map[static_cast<size_t>(x)][static_cast<size_t>(y)]) 
+				{
+					f.put(0xFF);
+					f.put(0x10);
+					f.put(0x10);
+				}
+				else
+				{
+					f.put(0x10);
+					f.put(0x10);
+					f.put(0x10);
+				}
 			}
 		}
 	}
